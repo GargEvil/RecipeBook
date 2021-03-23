b12d98091763e7a71e5982394fb5d9eaa32dec5d
Mahir Skula <mahir.skula96@hotmail.com>
2021-03-23 22:17:18 +0100

    adding content to recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.less']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipe: Recipe[] = [
+    new Recipe('Paprikaš',
+     'Ukusno bosansko jelo napravljeno od domacih proizvoda',
+      'https://www.recepti.com/img/recipe/41214-krompir-paprikas-sa-junetinom.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
