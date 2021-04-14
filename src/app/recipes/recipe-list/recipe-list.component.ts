cb82c7189f2b6e11e1705547618296418371c98f
Mahir Skula <mahir.skula96@hotmail.com>
2021-04-14 20:58:17 +0200

    Adding functionality to recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.model';
+import { Recipe } from './../recipe.model';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,14 +7,25 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.less']
 })
 export class RecipeListComponent implements OnInit {
-  recipe: Recipe[] = [
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
     new Recipe('Paprikaš',
      'Ukusno bosansko jelo napravljeno od domacih proizvoda',
-      'https://www.recepti.com/img/recipe/41214-krompir-paprikas-sa-junetinom.jpg')
+      'https://www.recepti.com/img/recipe/41214-krompir-paprikas-sa-junetinom.jpg'),
+      new Recipe('Paprikaš 2',
+      'Drugo ukusno bosansko jelo napravljeno od domacih proizvoda',
+       'https://www.recepti.com/img/recipe/41214-krompir-paprikas-sa-junetinom.jpg')
+   
   ];
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
